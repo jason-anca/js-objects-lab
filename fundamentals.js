f1f966c575afe9c16dc1cba1434ca8f1e0c43fff
jasonanca <jason_anca@hotmail.com>
2022-09-16 14:55:21 +0100

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,9 @@
 const aCar = {
     owner : "Joe Bloggs",
+    previous_owners: [
+      { name: "Pat Smith", address: "1 Main Street"},
+      { name: "Sheila Dwyer", address: "2 High Street"}
+    ],
     type : {
     make : 'Toyota',
     model : 'Corolla',
@@ -17,3 +21,17 @@ const aCar = {
   aCar.registration.year + "-" + 
   aCar.registration.county + "-" + 
   aCar.registration.number);
+
+  aCar.milage = 10000
+  aCar.color = {
+    exterior: "red",
+    interior: { texture: "leather", shade: "cream" }
+  }
+
+  console.log( 
+    "It is a " + aCar.color.exterior + 
+  " car, " + aCar.milage + 
+  " milage, with " + aCar.color.interior.texture + 
+  " interior.")
+
+  console.log('First owner : ' +  aCar.previous_owners[0].name + " - " + aCar.previous_owners[0].address)
